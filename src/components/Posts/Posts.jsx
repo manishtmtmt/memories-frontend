16ff4e0dc6c78e83ed95d6ec0f589bf4bfd02af4
manishtmtmt <manisht.mt.mt@gmail.com>
2023-03-05 13:04:58 +0530

    code push to github

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid, CircularProgress } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import useStyles from "./styles";
+import Post from "./Post/Post";
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+
+  const { classes } = useStyles();
+
+  return posts.length > 0 ? (
+    <Grid
+      className={classes.container}
+      container
+      alignItems={"stretch"}
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6} md={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  ) : (
+    <CircularProgress />
+  );
+};
+
+export default Posts;
